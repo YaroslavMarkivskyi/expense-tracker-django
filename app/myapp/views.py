b40f32ae902c330ef6555bcf56b3aef7684706cc
Yaroslav Markivskyi <yaroslav.markivskiy.it.2022@lpnu.ua>
2024-07-15 16:14:47 +0300

    feat: Save expense form data in index view
    This commit adds functionality to the index view to save the expense form data when the form is submitted. If the form is valid, the expense is saved to the database. This allows users to track their expenses within the application.

--- app/myapp/views.py
+++ app/myapp/views.py
@@ -5,5 +5,10 @@ from .forms import ExpenseForm
 
 
 def index(request):
+    if request.method == 'POST':
+        expense = ExpenseForm(request.POST)
+        if expense.is_valid():
+            expense.save()
+
     expense_form = ExpenseForm()
-    return render(request, 'myapp/index.html')
+    return render(request, 'myapp/index.html', {'expense_form': expense_form})
