266971ac62ae3630f71e4596cba068b29277dbda
Yaroslav Markivskyi <yaroslav.markivskiy.it.2022@lpnu.ua>
2024-07-15 17:11:54 +0300

    feat: Add logic to calculate expenses for different time periods
    This commit adds logic to the index view to calculate expenses for different time periods such as yearly, monthly, and weekly. It uses the `datetime` module to determine the start date for each time period and filters the expenses accordingly. The calculated sums are then displayed in the template. This feature enhances the functionality of the application by providing users with insights into their spending habits over different time intervals.

--- app/myapp/views.py
+++ app/myapp/views.py
@@ -1,4 +1,7 @@
+import datetime
+
 from django.shortcuts import render, redirect
+from django.db.models import Sum
 
 from .forms import ExpenseForm
 from .models import Expense
@@ -6,14 +9,48 @@ from .models import Expense
 
 
 def index(request):
-    if request.method == 'POST':
+    if request.method =="POST":
         expense = ExpenseForm(request.POST)
         if expense.is_valid():
             expense.save()
 
     expenses = Expense.objects.all()
+    total_expenses = expenses.aggregate(Sum('amount'))
+
+
+
+    #Logic to calculate 365 days expenses
+    last_year = datetime.date.today() - datetime.timedelta(days=365)
+    data = Expense.objects.filter(date__gt=last_year)
+    yearly_sum = data.aggregate(Sum('amount'))
+
+    #Logic to calculate 30 days expenses
+    last_month = datetime.date.today() - datetime.timedelta(days=30)
+    data = Expense.objects.filter(date__gt=last_month)
+    monthly_sum = data.aggregate(Sum('amount'))
+
+     #Logic to calculate 7 days expenses
+    last_week = datetime.date.today() - datetime.timedelta(days=7)
+    data = Expense.objects.filter(date__gt=last_week)
+    weekly_sum = data.aggregate(Sum('amount'))
+
+    daily_sums = Expense.objects.filter().values('date').order_by('date').annotate(sum=Sum('amount'))
+
+
+    categorical_sums = Expense.objects.filter().values('category').order_by('category').annotate(sum=Sum('amount'))
+    print(categorical_sums)
+
     expense_form = ExpenseForm()
-    return render(request, 'myapp/index.html', {'expense_form': expense_form, 'expenses': expenses})
+    return render(request,'myapp/index.html',{
+        'expense_form':expense_form,
+        'expenses':expenses,
+        'total_expenses':total_expenses,
+        'yearly_sum':yearly_sum,
+        'weekly_sum':weekly_sum,
+        'monthly_sum':monthly_sum,
+        'daily_sums':daily_sums,
+        'categorical_sums':categorical_sums
+        })
 
 
 def edit(request, id):
